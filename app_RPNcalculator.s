df21272834b071755c6c16f56df32ec5b0a9923c
Agustin Costabile <agustincostabile03@gmail.com>
2024-06-14 23:42:48 -0300

    v0.5.6

--- app_RPNcalculator.s
+++ app_RPNcalculator.s
@@ -3,15 +3,14 @@
     stack: .space 40
     stack_top: .word 0
     input_buffer: .word 0
-    input_internal1: .byte 0x0
-    input_internal2: .byte 0x0
-    input_internal3: .byte 0x0 
-    input_internal4: .byte 0x0
-    input_internal5: .byte 0x0
-    input_internal6: .byte 0x0 
-    input_internal7: .byte 0x0
-    input_internal8: .byte 0x0
-    input_internal9: .byte 0x0
+ 
+    input_internal: .byte 0x0 , 0x0, 0x0, 0x0
+    current_iteration: .byte 0x0
+    input_flag: .byte 0x0
+    
+    selected_operation: .byte 0x0
+    
+    
     buffer1: .word 0
     buffer2: .word 0
  
@@ -23,14 +22,82 @@
 	li $a1, 7
 	jal print_str
 	jal print_stack
-	loop_calc:
-	jal read_input
-	j loop_calc
-	j execCalc
-	
-	
-    
-	
+	read_input:
+	    jal keyboard_check
+	    
+	    beq $v0, $zero, read_input
+	    beq $v0, 0xF0, exit_calc
+	    beq $v0, 35, operation_select
+	    beq $v0, 0xF7, end_read
+	    li $t1, 0x1
+	    sb $t1, input_flag
+	    
+	    digit_management:
+		lb $t0, current_iteration
+		la $t1, input_internal
+		add $t1, $t1, $t0
+		addi $v0, $v0, -48
+		sb $v0, ($t1)
+		addi $t0, $t0, 1
+		beq $t0, 5, end_and_convert
+		sb $t0, current_iteration
+		j read_input
+		
+		
+		
+	    
+	    operation_select:
+		li $t1, 0x1
+		sb $t1, input_flag
+		lb $t0, selected_operation
+		addi $t0, $t0, 1
+		beq $t0, 5, start_over
+		j read_input
+		
+		start_over:
+		    lw $zero, operation_select
+		    j read_input
+		
+	    end_read:
+	    lb $t1, input_flag
+	    beq $t1, $zero, read_input
+	    end_and_convert:
+		lb $zero, input_flag
+		sw $zero, current_iteration
+		
+		lb $t0, selected_operation
+		bne $t0, $zero, end_operation
+		
+		
+		li $t0, 1
+		li $t1, 3
+		convert_loop:
+		    la $t2, input_internal
+		    add $t2, $t1, $t2
+		    lb $t3, ($t2)
+		    mul $t3, $t3, $t0
+		    lw $t2, input_buffer
+		    add $t2, $t3, $t2
+		    sw $t2, input_buffer
+		    
+		    addi $t1, $t1, -1
+		    mul $t0, 10
+		    mflo $t0
+		    bne $t1, -1, convert_loop
+		    
+	        lw $a0, input_buffer
+		jal stackPush
+		lw $zero, input_buffer
+		j execCalc
+		    
+	    end_operation:
+		# + (1), - (2), * (3), p (4)
+		lb $zero, input_flag
+		lb $a0, operation_select
+		sb $zero, operation_select
+		jal operate
+	    j execCalc
+
 	exit_calc:
 	    j goBack
     # push function
@@ -155,8 +222,10 @@
 	sw	    $s3, 16($sp)
     
     
-	lw $t0, buffer1
-	lw $t1, buffer2
+	jal stackPop
+	add $t0, $v0, $zero
+	jal stackPop
+	add $t1, $v0, $zero
 	beq $a0, 1, add_op
 	beq $a0, 2, sub_op
 	beq $a0, 3, mult_op
@@ -211,64 +280,3 @@
 	addi	    $sp, $sp, 20
 	jr	    $ra
 	
-
-    read_input:
-	addi	    $sp, $sp, -20
-	sw	    $ra, ($sp)
-	sw	    $s0, 4($sp)
-	sw	    $s1, 8($sp)
-	sw	    $s2, 12($sp)
-	sw	    $s3, 16($sp)
-	
-	jal keyboard_check
-	beq $v0, 0xF7, read_input
-	beq $v0, 0xF0, exit_calc
-	
-	li $s0, 1
-	read_loop:
-	    addi $t0, $v0, -48
-	    mul $t0, $s0, $t0
-	    lw $t1, input_buffer
-	    add $t0, $t1, $t0
-	    sw $t0, input_buffer
-	    
-	    add $a0, $v0, $zero
-	    # special print
-	    jal print_char
-	    jal keyboard_check
-	    beq $v0, 0xF7, end_read_loop
-	    mul $s0, $s0, 10
-	    bgt $s0, 0x10000000, end_read_loop
-	    j read_loop
-	    
-	end_read_loop:
-	    lw $a0, input_buffer
-	    jal stackPush
-	    lw $zero, input_buffer
-    
-	lw	    $s3, 16($sp)
-	lw	    $s2, 12($sp)
-	lw	    $s1, 8($sp)
-	lw	    $s0, 4($sp)
-	lw	    $ra, ($sp)
-	addi	    $sp, $sp, 20
-	jr	    $ra
-
-    
-    reWrite_input:
-	addi	    $sp, $sp, -20
-	sw	    $ra, ($sp)
-	sw	    $s0, 4($sp)
-	sw	    $s1, 8($sp)
-	sw	    $s2, 12($sp)
-	sw	    $s3, 16($sp)
-	
-	
-	
-	lw	    $s3, 16($sp)
-	lw	    $s2, 12($sp)
-	lw	    $s1, 8($sp)
-	lw	    $s0, 4($sp)
-	lw	    $ra, ($sp)
-	addi	    $sp, $sp, 20
-	jr	    $ra
