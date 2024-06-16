134b5bf42c2070b358fec2b140d2fc1348f24720
Agustin Costabile <agustincostabile03@gmail.com>
2024-06-16 20:03:22 -0300

    v0.6

--- app_RPNcalculator.s
+++ app_RPNcalculator.s
@@ -4,7 +4,7 @@
     stack_top: .word 0
     input_buffer: .word 0
  
-    input_internal: .byte 0x0 , 0x0, 0x0, 0x0
+    input_internal: .byte 0x0, 0x0, 0x0, 0x0
     current_iteration: .byte 0x0
     input_flag: .byte 0x0
     
@@ -15,7 +15,7 @@
     buffer1: .word 0
     buffer2: .word 0
     
-    str_current_type: .byte 0x5F, 0x5F, 0x5F, 0x5F
+    str_current_type: .byte 0x5F, 0x5F, 0x5F, 0x5F, 0x0
     str_stack: .asciiz "Stack: "
 .text
     execCalc:
@@ -25,12 +25,13 @@
 	jal print_str
 	jal print_stack
 	read_input:
+	    sw $zero, input_buffer
 	    jal print_typing
 	    jal keyboard_check
 	    
 	    beq $v0, $zero, read_input
 	    beq $v0, 0xF0, exit_calc
-	    beq $v0, 35, operation_select
+	    beq $v0, 42, operation_select
 	    beq $v0, 0xF7, end_read
 	    li $t1, 0x1
 	    sb $t1, input_flag
@@ -67,30 +68,39 @@
 	    beq $t1, $zero, read_input
 	    end_and_convert:
 		lb $zero, input_flag
-		sb $zero, current_iteration
 		
 		lb $t0, selected_operation
 		bne $t0, $zero, end_operation
 		
 		
 		li $t0, 1
-		li $t1, 3
+		lb $t1, current_iteration
+		la $t2, input_internal
 		convert_loop:
-		    la $t2, input_internal
-		    add $t2, $t1, $t2
 		    lb $t3, ($t2)
 		    mul $t3, $t3, $t0
-		    lw $t2, input_buffer
-		    add $t2, $t3, $t2
-		    sw $t2, input_buffer
+		    lw $t4, input_buffer
+		    add $t4, $t3, $t4
+		    sw $t4, input_buffer
 		    
 		    addi $t1, $t1, -1
+		    addi $t2, $t2, 1
 		    mul $t0, 10
 		    mflo $t0
-		    bne $t1, -1, convert_loop
+		    bne $t1, $zero, convert_loop
+		
+		    
+		la $t0, input_internal
+		li $t1, 0
+		reset_input_internal:
+		    add $t0, $t0, $t1
+		    sb $zero, ($t0)
+		    addi $t1, $t1, 1
+		    bne $t1, 4, reset_input_internal
 		    
 	        lw $a0, input_buffer
 		jal stackPush
+		sb $zero, current_iteration
 		lw $zero, input_buffer
 		la $t0, str_current_type
 		li $t1, 4
@@ -110,6 +120,8 @@
 		lb $a0, selected_operation
 		sb $zero, selected_operation
 		jal operate
+		add $a0, $v0, $zero
+		jal stackPush
 	    j execCalc
 
 	exit_calc:
@@ -196,18 +208,22 @@
 	sw	    $s3, 16($sp)
 	
 	
-	lw $t0, stack_top
-	li $t0, 0
+	lw $s0, stack_top
+	li $s1, 0
+	li $a0, 0x0B01
+	jal update_print_position
 	
 	print_loop:
-	    bge $t1, $t0, printStack_done
+	    bge $s1, $s0, printStack_done
 	    
 	    la $t2, stack
-	    sll $t3, $t1, 2
-	    add $a0, $t2, $zero
+	    sll $t3, $s1, 2
+	    add $t2, $t2, $t3
+	    lw $a0, ($t2)
 	    jal print_int
-	    li $a0, 20
+	    li $a0, 44
 	    jal print_char
+	    addi $s1, $s1, 1
 	    j print_loop
 	    
 	    
@@ -237,31 +253,31 @@
     
     
 	jal stackPop
-	add $t0, $v0, $zero
+	add $s0, $v0, $zero
 	jal stackPop
-	add $t1, $v0, $zero
+	add $s1, $v0, $zero
 	beq $a0, 1, add_op
 	beq $a0, 2, sub_op
 	beq $a0, 3, mult_op
 	beq $a0, 4, power_op
 	
 	add_op:
-	    add $v0, $t0, $t1
+	    add $v0, $s0, $s1
 	    j operate_end
 	sub_op:
-	    sub $v0, $t0, $t1
+	    sub $v0, $s0, $s1
 	    j operate_end
 	mult_op:
-	    mul $v0, $t0, $t1
+	    mul $v0, $s0, $s1
 	    j operate_end
 	power_op:
 	    li $v0, 1
 	    power_loop:
 		# t1 = exponent
 		# t0 = base
-		beq $t1, $zero, operate_end
-		mul $v0, $v0, $t0
-		addi $t1, $t1, -1
+		beq $s1, $zero, operate_end
+		mul $v0, $v0, $s0
+		addi $s1, $s1, -1
 		j power_loop
 	
 	operate_end:
@@ -306,8 +322,10 @@
 	la $t0, input_internal
 	la $t1, str_current_type
 	lb $t2, current_iteration
+	lb $t3, selected_operation
+	bne $t3, $zero, print_operation
 	copy_loop:
-	    beq	    $t2, $zero, waiting_dash
+	    beq	    $t2, $zero, ready_to_print
 	    lb	    $t3, ($t0)
 	    addi    $t3, $t3, 48
 	    sb	    $t3, ($t1)
@@ -316,13 +334,37 @@
 	    addi    $t2, $t2, -1
 	    j	    copy_loop
 	
-	waiting_dash:
-	    lb $t0, current_iteration
-	    la $t1, str_current_type
-	    add $t0, $t0, $t1
-	    li $t2, 0xFF
-	    sb $t2, ($t0)
-	
+	print_operation:
+	    beq $t3, 1, display_sum
+	    beq $t3, 2, display_sub
+	    beq $t3, 3, display_mult
+	    li $t4, 94
+	    sb $t4, ($t1)
+	    addi $t1, $t1, 1
+	    sb $zero, ($t1)
+	    j ready_to_print
+	    display_sum:
+		li $t4, 43
+		sb $t4, ($t1)
+		addi $t1, $t1, 1
+		sb $zero, ($t1)
+		j ready_to_print
+	    display_sub:
+		li $t4, 45
+		sb $t4, ($t1)
+		addi $t1, $t1, 1
+		sb $zero, ($t1)
+		j ready_to_print
+	    display_mult:
+		li $t4, 42
+		sb $t4, ($t1)
+		addi $t1, $t1, 1
+		sb $zero, ($t1)
+		j ready_to_print
+	    
+	    
+	    
+	ready_to_print:
 	la $a0, str_current_type
 	li $a1, 0x5F06
 	jal print_str
